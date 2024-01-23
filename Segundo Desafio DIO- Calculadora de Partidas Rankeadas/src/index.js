4e1538ac6a4bd0fc9c12c2cf3e1ec4257de8dd27
PDeiroz <pdeirozgestao.t.i@outlook.com.br>
2024-01-23 12:01:08 -0300

    Add files via upload

---
+++ Segundo Desafio DIO- Calculadora de Partidas Rankeadas/src/index.js
@@ -0,0 +1,30 @@
+function calcularSaldoVitorias(vitorias, derrotas) {
+    return vitorias - derrotas;
+}
+
+function calcularNivel(saldo) {
+    if (saldo <= 10) {
+        return "Ferro";
+    } else if (saldo <= 20) {
+        return "Bronze";
+    } else if (saldo <= 50) {
+        return "Prata";
+    } else if (saldo <= 80) {
+        return "Ouro";
+    } else if (saldo <= 90) {
+        return "Diamante";
+    } else if (saldo <= 100) {
+        return "Lendário";
+    } else {
+        return "Imortal";
+    }
+}
+
+// Exemplo de uso
+const quantidadeVitorias = 23;
+const quantidadeDerrotas = 7;
+
+const saldo = calcularSaldoVitorias(quantidadeVitorias, quantidadeDerrotas);
+const nivel = calcularNivel(saldo);
+
+console.log(`O Herói tem um saldo de ${saldo} e está no nível de ${nivel}.`);
